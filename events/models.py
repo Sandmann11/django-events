25f296c7aa27a08a4370e8d8d509f2160311c690
Arek P <sandmann@o2.pl>
2021-08-19 03:52:22 +0200

    fixed images

--- events/models.py
+++ events/models.py
@@ -56,7 +56,7 @@ class Event(models.Model):
     end_date = models.DateField('End Date')
     end_time = models.TimeField('End Time', blank=True, null=True)
     description = models.TextField(max_length=500, blank=True)
-    image = models.ImageField(upload_to='event/',blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
     lineup = models.ManyToManyField(Artist, related_name='gigs', blank=True)
     organizer = models.CharField(max_length=100, blank=True)
     website = models.URLField('Event Website', blank=True)
