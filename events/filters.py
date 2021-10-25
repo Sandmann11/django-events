df4b2acb8c06cf65de6b279112589dc8bc0190dc
Arek P <sandmann@o2.pl>
2021-10-25 15:18:22 +0200

    back on track

--- events/filters.py
+++
@@ -1,8 +0,0 @@
-# import django_filters
-# from .models import *
-
-
-# class EventFilter(django_filters.FilterSet):
-#     class Meta:
-#         model = Event
-#         fields = ['name', 'category', 'venue', 'start_date']
